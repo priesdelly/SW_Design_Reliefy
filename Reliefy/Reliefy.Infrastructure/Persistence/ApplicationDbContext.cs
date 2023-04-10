0cd31342cf8abb1984cced7a350daf9841d1db9e
Montawat Sripaiboon <montawat.gun@hotmail.com>
2023-04-11 01:34:10 +0700

    Refactor to use firebase auth

--- Reliefy/Reliefy.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Reliefy/Reliefy.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Reliefy.Application.Interfaces;
-using Reliefy.Domain.Entities;
 
 namespace Reliefy.Infrastructure.Persistence;
 
-public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>, IApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
 	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 	{
